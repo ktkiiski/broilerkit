0bdd4c9fd40f956d7beaa034a1f4b42ce323d3dc
Kimmo Kiiski <kimmo@kii.ski>
2017-08-08 23:08:42 +0300

    Add types for version constants (by DefinePlugin)

---
+++ types/constants.d.ts
@@ -0,0 +1,15 @@
+/**
+ * GIT commit hash of the web app build.
+ */
+declare var __COMMIT_HASH__: string;
+
+/**
+ * The version of the app as described by
+ * the GIT command `git describe`.
+ */
+declare var __VERSION__: string;
+
+/**
+ * The GIT branch that was used to build the app.
+ */
+declare var __BRANCH__: string;
