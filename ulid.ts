574767eb4201021ae632f8ef4018ea447bf23363
Kimmo Kiiski <kimmo@kii.ski>
2018-03-25 21:54:28 +0300

    Wrapped `ulid` library as a module

---
+++ ulid.ts
@@ -0,0 +1,25 @@
+import * as ULID from 'ulid';
+
+const ulidFactory = ULID.monotonicFactory();
+
+/**
+ * Generates a new ULID identifier string and returns it.
+ * The identifier is guaranteed to be higher than returned
+ * by the previous call.
+ *
+ * By default the current time is used as a seed, but you
+ * can optionally provide a timestamp which will be used.
+ *
+ * @param timestamp Optional time seed, either integer or Date
+ */
+export function ulid(timestamp?: number | Date) {
+    return ulidFactory(timestamp && +timestamp);
+}
+
+export function decodeTime(id: string): number {
+    return ULID.decodeTime(id);
+}
+
+export function decodeDate(id: string): Date {
+    return new Date(decodeTime(id));
+}
