c52143a21971c2f1efd74d9e6ffcfab1da31e9f8
Kimmo Kiiski <kimmo@kii.ski>
2018-04-29 19:38:22 +0300

    Do not generate TypeScript declaration files

--- auth-local-api.ts
+++ auth-local-api.ts
@@ -1,12 +1,8 @@
-import { ApiEndpoint, CreateEndpoint, CreateEndpointMethodMapping, endpoint, ListEndpoint, ListEndpointMethodMapping, OptionsEndpointMethodMapping } from './api';
-import { User, userResource } from './users';
+import { endpoint } from './api';
+import { userResource } from './users';
 
 // tslint:disable-next-line:variable-name
-export const _usersCollection: ApiEndpoint<User, never, CreateEndpoint<Pick<User, 'name' | 'email'> & Partial<Pick<User, never>>, Pick<User, 'name' | 'email'> & Partial<Pick<User, never>>, User> & ListEndpoint<{
-    ordering: 'name';
-    direction: 'asc' | 'desc';
-    since?: string | undefined;
-} & Pick<User, never>, User>, CreateEndpointMethodMapping<'none'> & ListEndpointMethodMapping<'none'> & OptionsEndpointMethodMapping> = endpoint(userResource)
+export const _usersCollection = endpoint(userResource)
     .url `/_users`
     .listable({
         orderingKeys: ['name'],
--- auth-local-server.ts
+++ auth-local-server.ts
@@ -1,16 +1,10 @@
-import { CreateEndpoint, CreateEndpointMethodMapping, ListEndpoint, ListEndpointMethodMapping, OptionsEndpointMethodMapping } from './api';
 import * as localApi from './auth-local-api';
 import { Created } from './http';
-import { EndpointImplementation, implement } from './server';
-import { User } from './users';
+import { implement } from './server';
 import { uuid4 } from './uuid';
 
 // tslint:disable-next-line:variable-name
-export const _usersCollection: EndpointImplementation<{}, CreateEndpoint<Pick<User, 'name' | 'email'> & Partial<Pick<User, never>>, Pick<User, 'name' | 'email'> & Partial<Pick<User, never>>, User> & ListEndpoint<{
-    ordering: 'name';
-    direction: 'asc' | 'desc';
-    since?: string | undefined;
-} & Pick<User, never>, User>, CreateEndpointMethodMapping<'none'> & ListEndpointMethodMapping<'none'> & OptionsEndpointMethodMapping> = implement(localApi._usersCollection, {})
+export const _usersCollection = implement(localApi._usersCollection, {})
     .list(async ({ordering, direction, since}, {users}) => {
         return since ? [] : await users.list({
             ordering, direction,
