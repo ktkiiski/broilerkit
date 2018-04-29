79b9da95c9152606bce544d5e2b26bc81b0970b2
Kimmo Kiiski <kimmo@kii.ski>
2018-04-29 19:38:49 +0300

    Fixed JWT decoding issue

--- jwt.ts
+++ jwt.ts
@@ -10,6 +10,6 @@ export interface JWTPayload {
  */
 export function parseJwt(token: string): JWTPayload {
     const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     return JSON.parse(window.atob(base64));
 }
