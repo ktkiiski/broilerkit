399fd799824f51a387e0036ec83dfaf29b05ebc8
Kimmo Kiiski <kimmo@kii.ski>
2018-06-19 22:53:44 +0300

    Comments for router methods

--- routing.ts
+++ routing.ts
@@ -17,14 +17,31 @@ export class Router<T> extends Observable<State<T> | null> {
             ;
         });
     }
+    /**
+     * Changes to the given state, with the given parameters,
+     * adding it to the browsing history.
+     * @param routeName name of the state to transition to
+     * @param params parameters for the transitioned state
+     */
     public push<K extends keyof T>(routeName: K, params: T[K]): void {
         const routing = this.routings[routeName];
         this.location.push(routing.compile(params).toString());
     }
+    /**
+     * Changes to the given state, with the given parameters,
+     * replacing the topmost state in the browsing history.
+     * @param routeName name of the state to transition to
+     * @param params parameters for the transitioned state
+     */
     public replace<K extends keyof T>(routeName: K, params: T[K]): void {
         const routing = this.routings[routeName];
         this.location.replace(routing.compile(params).toString());
     }
+    /**
+     * Parses the given URL and returns the routing state that it matches.
+     * If the URL matches no defined route, returns null.
+     * @param url URL to parse
+     */
     public match(url: string): State<T> | null {
         const routings = this.routings;
         for (const name in routings) {
