943a28a0c43441447398e3e75930854f3cc1e3c2
Kimmo Kiiski <kimmo@kii.ski>
2018-07-06 23:15:53 +0300

    Fixed UUID field validation error message

--- fields.ts
+++ fields.ts
@@ -226,7 +226,7 @@ class UUIDField extends RegexpField {
     constructor(version?: 1 | 4 | 5) {
         super(
             new RegExp(`^[0-9a-f]{8}-[0-9a-f]{4}-[${version || '145'}][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$`, 'i'),
-            version ? `Value is not a valid UUID` : `Value is not a valid UUID version ${version}`,
+            version ? `Value is not a valid UUID version ${version}` : `Value is not a valid UUID`,
         );
     }
 }
