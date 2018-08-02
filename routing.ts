8c02fe0ba2ce4a5e13da5e318e726939bab81cfe
Kimmo Kiiski <kimmo@kii.ski>
2018-08-02 16:06:32 +0300

    Methods for getting compiled URLs for a route state

--- routing.ts
+++ routing.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Location } from './location';
 import { Route } from './routes';
+import { Url } from './url';
 import { Key } from './utils/objects';
 
 export interface State<T, K extends Key<T> = Key<T>> {
@@ -25,8 +26,7 @@ export class Router<T> extends Observable<State<T> | null> {
      * @param params parameters for the transitioned state
      */
     public push<K extends keyof T>(routeName: K, params: T[K]): void {
-        const routing = this.routings[routeName];
-        this.location.push(routing.compile(params).toString());
+        this.location.push(this.buildUrl(routeName, params));
     }
     /**
      * Changes to the given state, with the given parameters,
@@ -35,8 +35,7 @@ export class Router<T> extends Observable<State<T> | null> {
      * @param params parameters for the transitioned state
      */
     public replace<K extends keyof T>(routeName: K, params: T[K]): void {
-        const routing = this.routings[routeName];
-        this.location.replace(routing.compile(params).toString());
+        this.location.replace(this.buildUrl(routeName, params));
     }
     /**
      * Parses the given URL and returns the routing state that it matches.
@@ -58,5 +57,23 @@ export class Router<T> extends Observable<State<T> | null> {
             }
         }
         return null;
+    }
+    /**
+     * Returns an URL string (with path and any query parameters)
+     * for the given router state name and parameters.
+     * @param routeName name of the state
+     * @param params parameters required by the state
+     */
+    public buildUrl<K extends keyof T>(routeName: K, params: T[K]): string {
+        return this.compileUrl(routeName, params).toString();
+    }
+    /**
+     * Returns an URL object for the given router state name and parameters.
+     * @param routeName name of the state
+     * @param params parameters required by the state
+     */
+    public compileUrl<K extends keyof T>(routeName: K, params: T[K]): Url {
+        const routing = this.routings[routeName];
+        return routing.compile(params);
     }
 }
