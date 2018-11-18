af4ddbff40220fc61a25049a18e30dcf2e2e8b32
Kimmo Kiiski <kimmo@kii.ski>
2018-11-18 17:26:04 +0200

    Resources are named

--- resources.ts
+++ resources.ts
@@ -3,30 +3,36 @@ import { Key, pick, spread } from './utils/objects';
 
 export class Resource<T, PK extends Key<T>, V extends Key<T> | undefined> extends FieldSerializer<T> {
     /**
+     * @param name The identifying name of this type of resource.
      * @param fields Attribute names with their field definitions of the resource.
      * @param identifyBy Attributes whose values together uniquely identify resources.
      * @param versionBy Attribute whose value can be used to determine if the item's has updated.
      */
-    constructor(fields: Fields<T>, public readonly identifyBy: PK[], public readonly versionBy: V) {
+    constructor(
+        public readonly name: string,
+        fields: Fields<T>,
+        public readonly identifyBy: PK[],
+        public readonly versionBy: V) {
         super(fields);
     }
     public expose<K extends Exclude<Key<T> & Key<Fields<T>>, PK>>(attrs: K[]): Resource<Pick<T, K | PK>, PK, V extends K ? V : undefined> {
         const versionBy = attrs.indexOf(this.versionBy as any) >= 0 ? this.versionBy : undefined;
-        return new Resource(pick(this.fields, [...attrs, ...this.identifyBy]) as Fields<Pick<T, K | PK>>, this.identifyBy, versionBy as V extends K ? V : undefined);
+        return new Resource(this.name, pick(this.fields, [...attrs, ...this.identifyBy]) as Fields<Pick<T, K | PK>>, this.identifyBy, versionBy as V extends K ? V : undefined);
     }
     public expand<E>(fields: Fields<E>): Resource<T & E, PK, V> {
-        return new Resource(spread(this.fields, fields) as Fields<T & E>, this.identifyBy, this.versionBy);
+        return new Resource(this.name, spread(this.fields, fields) as Fields<T & E>, this.identifyBy, this.versionBy);
     }
 }
 
 export type Deserialization<T extends Serializer<any, any>> = T extends Serializer<infer R> ? R : any;
 
 interface ResourceOptions<T, PK extends Key<T>, V extends Key<T> | undefined> {
+    name: string;
     fields: Fields<T>;
     identifyBy: PK[];
     versionBy?: V;
 }
 
 export function resource<T, PK extends Key<T>, V extends Key<T> | undefined = undefined>(options: ResourceOptions<T, PK, V>): Resource<T, PK, V> {
-    return new Resource(options.fields, options.identifyBy, options.versionBy as V);
+    return new Resource(options.name, options.fields, options.identifyBy, options.versionBy as V);
 }
--- users.ts
+++ users.ts
@@ -11,6 +11,7 @@ export interface User {
 }
 
 export const user: Resource<User, 'id', 'updatedAt'> = resource({
+    name: 'user',
     fields: {
         id: uuid(),
         name: string(),
