7bb6ed5c39229931fcc2c7ad983ba14fef271188
Kimmo Kiiski <kimmo@kii.ski>
2019-02-03 20:22:34 +0200

    Helpers for grouping values from an array

---
+++ utils/groups.ts
@@ -0,0 +1,20 @@
+export function groupBy<K extends string, T>(values: T[], selector: (item: T) => K): {[key in K]: T[]} {
+    const results: {[key: string]: T[]} = {};
+    for (const value of values) {
+        const key = selector(value);
+        const acc = results[key] = results[key] || [] as T[];
+        acc.push(value);
+    }
+    return results;
+}
+
+export function groupByAsMap<K extends string, T>(values: T[], selector: (item: T) => K): Map<K, T[]> {
+    const results = new Map<K, T[]>();
+    for (const value of values) {
+        const key = selector(value);
+        const acc = results.get(key) || [] as T[];
+        results.set(key, acc);
+        acc.push(value);
+    }
+    return results;
+}
