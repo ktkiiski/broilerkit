2cb825a4de37816cd8472dc73ec8f4c826a72ed9
Kimmo Kiiski <kimmo@kii.ski>
2019-05-25 12:53:22 +0300

    Upgraded core-js

--- polyfill/promise.ts
+++ polyfill/promise.ts
@@ -1,6 +1,4 @@
-// Intentional side effects: ensure that core-js has backfilled Promise
-import 'core-js/library/modules/es6.promise';
 /**
  * Export the native Promise, if available, or otherwise the polyfill implementation.
  */
-export const Promise: PromiseConstructor = require('core-js/library/modules/_core').Promise;
+export const Promise: PromiseConstructor = require('core-js-pure/features/promise').default;
--- polyfill/symbol.ts
+++ polyfill/symbol.ts
@@ -1,7 +1,6 @@
-// Intentional side effects: ensure that core-js has backfilled Symbol
-import 'core-js/library/modules/es6.symbol';
-import 'core-js/library/modules/es7.symbol.async-iterator';
+// Intentional side effects: ensure that async iterator Symbol is defined
+import 'core-js-pure/features/symbol/async-iterator.js';
 /**
  * Export the native Symbol, if available, or otherwise the polyfill implementation.
  */
-export const Symbol: SymbolConstructor = require('core-js/library/modules/_core').Symbol;
+export const Symbol: SymbolConstructor = require('core-js-pure/features/symbol').default;
