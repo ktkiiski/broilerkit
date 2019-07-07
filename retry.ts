6954b5c415574bbe5a6bbce9aac7c3e81756072a
Kimmo Kiiski <kimmo@kii.ski>
2019-07-07 21:16:47 +0300

    Added helpers for retrying actions

---
+++ retry.ts
@@ -0,0 +1,89 @@
+import { wait } from './async';
+import { HttpStatus, isErrorResponse } from './http';
+
+const retryableStatusCodes = [
+    HttpStatus.ServiceUnavailable,
+    HttpStatus.RequestTimeout,
+    HttpStatus.TooManyRequests,
+    HttpStatus.GatewayTimeout,
+];
+
+/**
+ * Executes the given (async) function, running it again if it
+ * throws an exception that represents a retryable HTTP error status.
+ * The retry attempts are delayed with an exponential backoff mechanism
+ * with automatically calculated duration. Retrying is done maximum
+ * of the given number of times.
+ * @param maxRetryCount Maximum number of retry attempts
+ * @param fn The function to execute
+ */
+export async function retryWithBackoff<T>(maxRetryCount: number, fn: () => Promise<T>, statusCodes = retryableStatusCodes): Promise<T> {
+    const startTime = new Date().getTime();
+    while (true) {
+        try {
+            return await fn();
+        } catch (error) {
+            if (maxRetryCount <= 0 || !isErrorResponse(error)) {
+                // No more retries or not retryable error
+                throw error;
+            }
+            const { statusCode } = error;
+            if (statusCodes.indexOf(statusCode) < 0) {
+                // Not a retryable status code
+                throw error;
+            }
+            // Retry
+            const { 'Retry-After': retryAfter } = error.headers;
+            maxRetryCount -= 1;
+            // Wait for a random portion of the total time spent
+            await wait(getRetryDelay(startTime, retryAfter));
+        }
+    }
+}
+
+function getRetryDelay(startTime: number, retryAfter: string | undefined): number {
+    const now = new Date().getTime();
+    if (retryAfter) {
+        const retryTimestamp = new Date(retryAfter).getTime();
+        if (!Number.isNaN(retryTimestamp)) {
+            // Retry-After is a valid date/time
+            return Math.max(0, now - retryTimestamp);
+        }
+        const retryDelay = parseFloat(retryAfter);
+        if (!Number.isNaN(retryDelay)) {
+            return Math.max(0, retryDelay * 1000);
+        }
+    }
+    // Otherwise get a random portion from the duration passed so far
+    const totalDuration = now - startTime;
+    return Math.floor(Math.random() * totalDuration);
+}
+
+const conflictStatusCodes = [
+    HttpStatus.PreconditionFailed,
+    HttpStatus.Conflict,
+];
+
+/**
+ * Utility for performing a simple conditional update using
+ * the given function. The function will be run again whenever
+ * it raises a 409 or 412 or error.
+ * @param fn Function that performs a "transaction"
+ */
+export async function retryOptimistically<T>(fn: () => Promise<T>, statusCodes = conflictStatusCodes): Promise<T> {
+    while (true) {
+        try {
+            return await fn();
+        } catch (error) {
+            if (isErrorResponse(error)) {
+                const { statusCode } = error;
+                if (statusCodes.indexOf(statusCode) >= 0) {
+                    // There was a conflict. Try again.
+                    continue;
+                }
+            }
+            // Pass the error through
+            throw error;
+        }
+    }
+}
