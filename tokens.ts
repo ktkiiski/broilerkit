82a582ae9ad804a795f845cffc7eae09abbe009c
Kimmo Kiiski <kimmo@kii.ski>
2019-10-09 19:55:09 +0300

    Replace `msgpack` with `msgpack-lite`

--- tokens.ts
+++ tokens.ts
@@ -1,13 +1,13 @@
-import * as msgpack from 'msgpack';
+import * as msgpack from 'msgpack-lite';
 import { JWE, JWK } from 'node-jose';
 
 export async function encryptToken(payload: {[key: string]: any}, secretKey: JWK.Key): Promise<string> {
-    const packaged = msgpack.pack(payload) as Buffer;
+    const packaged = msgpack.encode(payload);
     return JWE.createEncrypt({ format: 'compact' }, secretKey)
         .update(packaged).final();
 }
 
 export async function decryptToken(token: string, keyStore: JWK.KeyStore): Promise<any> {
     const decryption = await JWE.createDecrypt(keyStore).decrypt(token);
-    return msgpack.unpack(decryption.plaintext);
+    return msgpack.decode(decryption.plaintext);
 }
