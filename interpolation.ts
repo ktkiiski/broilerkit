4702d11eead3d7fb8da6a67e17fcbad2d0448bbf
Kimmo Kiiski <kimmo@kii.ski>
2019-10-10 18:19:18 +0300

    Interpolation helpers

---
+++ interpolation.ts
@@ -0,0 +1,21 @@
+/**
+ * Creates a simple templater function for filling the placeholders.
+ * This is intended to be used as a literal template tag:
+ *
+ *      const interpolate = tmpl `Hi, ${'name'}!`;
+ *      const str = interpolate({name: 'John'});
+ *      console.log(str);
+ *
+ * The example above would print "Hi, John!"
+ */
+export function tmpl<T extends string = never>(strings: TemplateStringsArray, ...placeholders: T[]) {
+    function interpolate(values: Record<T, string | number>) {
+        const chunks = strings.slice();
+        placeholders.forEach((placeholder, index) => {
+            const value = values[placeholder];
+            chunks.splice(1 + index * 2, 0, String(value));
+        });
+        return chunks.join('');
+    }
+    return interpolate;
+}
---
+++ spec/interpolation.spec.ts
@@ -0,0 +1,27 @@
+import * as assert from 'assert';
+import { tmpl } from '../interpolation';
+
+describe('tmpl', () => {
+    it('fills placeholders', () => {
+        const interpolate = tmpl `Hello ${'name1'} and ${'name2'}!`;
+        assert.equal(
+            interpolate({ name1: 'John', name2: 'Jane' }),
+            'Hello John and Jane!',
+        );
+    });
+    it('stringifies numbers', () => {
+        const interpolate = tmpl `${'name'} is ${'age'} years old`;
+        assert.equal(
+            interpolate({ name: 'John', age: 20 }),
+            'John is 20 years old',
+        );
+    });
+    it('works with just a placeholder', () => {
+        const interpolate = tmpl `${'message'}`;
+        assert.equal(interpolate({ message: 'Yay!' }), 'Yay!');
+    });
+    it('does not require placeholders', () => {
+        const interpolate = tmpl `Hello!`;
+        assert.equal(interpolate({}), 'Hello!');
+    });
+});
